e25ac85485a4eadc2f24d911e75e8e4c0ef6e522
Kimdev254 <your-email@example.com>
2024-10-10 12:34:02 +0300

    initial commit

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const cors = require("cors")
 const dotenv = require("dotenv")
 
 const app = express()
+app.use(express.static(__dirname + "/public"))
 app.use(express.json())
 app.use(cors())
 dotenv.config()
 
-// connect to the database
+// Connect to the database
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -16,15 +17,75 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
 })
 
-//Check if DB connection works
+// Check if connection is successful
 db.connect((err) => {
-  if (err) return console.log("Error connecting to mysql db")
-
-  console.log("Connected to mysql db as id:", db.threadId)
+  if (err) return console.log("Error connecting mysql db")
+  console.log("Connected to mysql as id:", db.threadId)
 })
 
-//listen to the server
-const port = process.env.DB_PORT
+// Set up view engine
+app.set("view engine", "ejs")
+app.set("views", __dirname + "/views")
+
+// Question 1: Retrieve all patients
+app.get("/patients", (req, res) => {
+  db.query("SELECT * FROM patients", (err, results) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send("Error retrieving data")
+    } else {
+      res.render("data", { results: results })
+    }
+  })
+})
+
+// Question 2: Retrieve all providers
+app.get("/providers", (req, res) => {
+  db.query("SELECT * FROM providers", (err, results) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send("Error retrieving data")
+    } else {
+      res.render("providers", { results: results })
+    }
+  })
+})
+
+// Question 3: Retrieve Patients by their first names
+app.get("/patients/firstname", (req, res) => {
+  db.query("SELECT first_name FROM patients", (err, results) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send("Error retrieving patients")
+    } else {
+      if (results.length > 0) {
+        res.render("firstnames", { firstNames: results }) // Render a new template with first names
+      } else {
+        res.status(404).send("No patients found")
+      }
+    }
+  })
+})
+
+// Question 4: Retrieve all providers by their specialty
+app.get("/providers/specialty", (req, res) => {
+  db.query("SELECT DISTINCT provider_specialty FROM providers", (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error retrieving specialty");
+    } else {
+      if (results.length > 0) {
+        res.render("specialty", { specialty: results });
+      } else {
+        res.status(404).send("Specialty not found");
+      }
+    }
+  });
+});
+
+
+// Server listening
+const port = process.env.PORT
 app.listen(port, () => {
-  console.log(`Server runnign on http:${port}`)
+  console.log(`Server running on http://localhost:${port}`)
 })
